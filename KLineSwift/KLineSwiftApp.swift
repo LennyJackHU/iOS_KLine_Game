c81f8b130ea5e574514a2ecbb54cb9af222d27c0
jackhu <huyijia1221@outlook.com>
2025-09-12 16:56:12 +0800

    Initial Commit

---
+++ KLineSwift/KLineSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  KLineSwiftApp.swift
+//  KLineSwift
+//
+//  Created by YiJia Hu on 2025/9/12.
+//
+
+import SwiftUI
+
+@main
+struct KLineSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KLineSwiftTests/KLineSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  KLineSwiftTests.swift
+//  KLineSwiftTests
+//
+//  Created by YiJia Hu on 2025/9/12.
+//
+
+import Testing
+
+struct KLineSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
